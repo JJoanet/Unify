e0eada14e96c6eae8309667bd6e634126109af09
Matt Reisdorf <mreisdorf9717@gmail.com>
2021-04-13 11:17:30 -0400

    mailto form

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,34 @@
 $("document").ready(function(){
     $(".dropdown-trigger").dropdown();     
 })
+
+
+
+
+
+let firstName = $("#first-name");
+let lastName = $("#last-name");
+let password = $("#password");
+let email = $("#email");
+let form = $("#form");
+let submitBtn = $("#submit");
+
+
+
+
+$("form").on("submit", function(event){
+    event.preventDefault();
+    console.log("form submitted");
+    
+    console.log(firstName.val());
+    console.log(lastName.val())
+    console.log(password.val());
+    console.log(email.val());
+
+    let message = "Hi, my name is " + firstName.val() + " " + lastName.val() + ".";
+    let subject = "Union Canvassing";
+    console.log(message);
+    + "Please get back to me at EMAIL or PHONE NUMBER.",
+    window.open("mailto:mreisdorf9717@gmail.com?subject="+subject+"&body="+message+"\r\n some more text. I wanted to reach out to you regarding canvassing for UNION. Please get back to me at EMAIL or PHONE NUMBER. Thanks");
+})
+
